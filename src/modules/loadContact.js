c1a033956aa2b2bae33bbbe5af29cc3d6915572a
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-04 21:38:13 +0530

    add a contact form to the contact us page

--- src/modules/loadContact.js
+++ src/modules/loadContact.js
@@ -1,3 +1,50 @@
+function inputContainer(label, input) {
+    const container = document.createElement("div");
+    container.classList = "input-container";
+    container.appendChild(label);
+    container.appendChild(input);
+    return container;
+}
+
+function generateFormDiv(){
+    const form = document.createElement("form");
+    form.id = "contact-form";
+
+    const nameInput = document.createElement("input");
+    nameInput.id = "name";
+    const nameInputLabel = document.createElement("label");
+    nameInputLabel.for = "name";
+    nameInputLabel.textContent = "Enter Name:";
+    form.appendChild(inputContainer(nameInputLabel, nameInput));
+
+    const mailInput = document.createElement("input");
+    mailInput.id = "email";
+    mailInput.type = "email";
+    const mailInputLabel = document.createElement("label");
+    mailInputLabel.for = "email";
+    mailInputLabel.textContent = "Enter your e-mail:";
+    form.appendChild(inputContainer(mailInputLabel, mailInput));
+
+    const msgInput = document.createElement("textarea");
+    msgInput.id = "msg";
+    msgInput.rows = 5;
+    msgInput.cols = 30;
+    const msgInputLabel = document.createElement("label");
+    msgInputLabel.for = "msg";
+    msgInputLabel.textContent = "Enter your message:";
+    form.appendChild(inputContainer(msgInputLabel, msgInput));
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList = "button-container";
+    const formSubmitBut = document.createElement("button");
+    formSubmitBut.type = "submit";
+    formSubmitBut.textContent = "Submit";
+    buttonContainer.appendChild(formSubmitBut);
+    form.appendChild(buttonContainer);
+
+    return form;
+}
+
 export const loadContact = () => {
     const contentDiv = document.querySelector("#content");
 
@@ -10,4 +57,9 @@ export const loadContact = () => {
     heading.classList = "page-title";
     heading.textContent = "Contact Us";
     mainDiv.appendChild(heading);
+
+    const form = generateFormDiv();
+    mainDiv.appendChild(form)
+
+
 }
